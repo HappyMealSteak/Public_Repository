a350802f86e206caecebf25161ab5f21d11afb71
HappyMealSteak <Tyler.Poniatowski@gmail.com>
2023-04-15 11:22:12 -0400

    Add files via upload

---
+++ Powershell/Download_Windows_Server_2016_ISO.ps1
@@ -0,0 +1,22 @@
+﻿# Define the URL for the Windows Server 2016 ISO from the official Microsoft source
+$url = "https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US"
+
+# Define the destination path for the downloaded ISO
+$destination = "C:\Downloads\Windows_Server_2016.iso"
+
+# Check if the destination folder exists, if not create it
+$destinationFolder = Split-Path -Path $destination -Parent
+if (-not (Test-Path $destinationFolder)) {
+    New-Item -Path $destinationFolder -ItemType Directory
+}
+
+# Download the ISO using Invoke-WebRequest
+Write-Host "Downloading Windows Server 2016 ISO..."
+Invoke-WebRequest -Uri $url -OutFile $destination
+
+# Verify the download
+if (Test-Path $destination) {
+    Write-Host "Download completed successfully!"
+} else {
+    Write-Host "Download failed. Please check the URL and try again."
+}
