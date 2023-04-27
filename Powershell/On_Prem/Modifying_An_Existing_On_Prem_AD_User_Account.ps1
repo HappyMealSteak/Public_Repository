f536e5f3022f87e9668659b163fd514eae634ad8
HappyMealSteak <Tyler.Poniatowski@gmail.com>
2023-04-27 16:43:05 -0700

    Delete Modifying_An_Existing_On_Prem_AD_User_Account.ps1

--- Powershell/On_Prem/Modifying_An_Existing_On_Prem_AD_User_Account.ps1
+++
@@ -1,7 +0,0 @@
-# Set the variables for the user account to modify
-$UserName = "JohnDoe"
-$Description = "Updated user account for John Doe"
-$EmailAddress = "johndoe@contoso.com"
-
-# Modify the user account
-Set-ADUser -Identity $UserName -Description $Description -EmailAddress $EmailAddress
